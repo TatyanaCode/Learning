90d809c88b2e4dada163f9d64a7ad31c1dfacafd
danilov.fedor <danilov.fedor@otr.ru>
2022-01-20 11:53:33 +0300

    массивы

--- src/ru/learn/java/Learn2.java
+++ src/ru/learn/java/Learn2.java
@@ -1,11 +1,31 @@
 package ru.learn.java;
 
+
+import java.util.Scanner;
+
 public class Learn2 {
 
     public static void main(String[] args) {
+        // массивы данных
 
+
+        int[] arr = new int[4];
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("Введите число");
+            int value = scanner.nextInt();
+            arr[i] = value;
+        }
+
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+            System.out.println("Minimal" + min);
+        }
     }
-
 }
 
 
