a4a875f68c335072bdab6ceb2f3d65ff27fdb8fa
danilov.fedor <danilov.fedor@otr.ru>
2022-01-30 16:37:08 +0300

    Квадратное уравнение

--- src/ru/learn/java/Learn2.java
+++ src/ru/learn/java/Learn2.java
@@ -2,23 +2,59 @@ package ru.learn.java;
 
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Learn2 {
 
     public static void main(String[] args) {
-        Scanner scanner= new Scanner(System.in);
-        int k,x,b,y;
-        System.out.println("Введите число k");
+        List<Point> points = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+        String console;
+        do{
+            System.out.println("Введите координату X");
+            console = scanner.nextLine();
+            try{
+                double x = Double.parseDouble(console);
+                System.out.println("Введите координату Y");
+                console = scanner.nextLine();
+                double y = Double.parseDouble(console);
+                points.add(new Point(x, y));
+            } catch (Exception e){
+                System.out.println("Введена неверная координата");
+            }
+        }
+        while(!console.equals("N"));
 
-        k = scanner.nextInt();
-        System.out.println("Введите число x");
-        x=  scanner.nextInt();
-        System.out.println("Введите число b");
-        b=scanner.nextInt();
-        y = k*x+b;
-        System.out.println("число Y "+y);
-         }
 
+        Point barycenter = barycenter(points);
 
+        System.out.println("Координата X = " + barycenter.getX());
+        System.out.println("Координата Y = " + barycenter.getY());
+
+//        int k, x, b, y;
+//        System.out.println("Введите число k");
+//
+//        k = scanner.nextInt();
+//        System.out.println("Введите число x");
+//        x = scanner.nextInt();
+//        System.out.println("Введите число b");
+//        b = scanner.nextInt();
+//        y = k * x + b;
+//        System.out.println("число Y " + y);
+    }
+
+    /*
+        Метод для нахождения барицентра фигуры
+        */
+    static public Point barycenter(List<Point> coordinates) {
+        double x = 0;
+        double y = 0;
+        for (Point item : coordinates) {
+            x += item.getX();
+            y += item.getY();
+        }
+        int listSize = coordinates.size();
+        return new Point((x / listSize), (y / listSize));
+    }
 }
---
+++ src/ru/learn/java/Square.java
@@ -0,0 +1,40 @@
+package ru.learn.java;
+
+import java.util.Scanner;
+
+public class Square {
+    public static void main(String[] args) {
+        double a, b, c, d;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            try {
+                System.out.println("Введите значение \"a\" ");
+                a = scanner.nextDouble();
+                System.out.println("Введите значение \"b\" ");
+                b = scanner.nextDouble();
+                System.out.println("Введите значение \"c\" ");
+                c = scanner.nextDouble();
+                // D=b^{2}-4ac.
+                d = b * b - 4 * a * c;
+                System.out.println("Дискриминант D = " + d);
+                if (d < 0) {
+                    System.out.println("Нет корней");
+                } else {
+                    double x1;
+                    double x2;
+                    x1 = (-b + Math.sqrt(d)) / (2 * a);
+                    x2 = (-b - Math.sqrt(d)) / (2 * a);
+                    System.out.println("Корни уравнения:");
+                    if (d != 0) {
+                        System.out.println("x1 = " + x1);
+                        System.out.println("x2 = " + x2);
+                    } else {
+                        System.out.println("x = " + x1);
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        } while (!scanner.nextLine().equals("N"));
+    }
+}
